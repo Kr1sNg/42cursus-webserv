ea8553e483f65429f67e9694abc70a5dfb259ae9
Theo B <theo.bahin@gmail.com>
2025-09-28 23:33:04 +0200

    update class locationconfig

--- webserv/conf/theo/Locationconfig.hpp
+++ webserv/conf/theo/Locationconfig.hpp
@@ -12,6 +12,20 @@ class Locationconfig
         std::vector<std::string> _methods;
         std::string _cgi_pass;
         std::string _redirect;
+
+        enum _directiveFlag
+        {
+            INDEX = 1 << 0,
+            ROOT = 1 << 1,
+            AUTOINDEX = 1 << 2,
+            METHODS = 1 << 3,
+            CGIPASS = 1 << 4,
+            REDIRECT = 1 << 5,
+        };
+
+        int _flags;
+        const std::map<std::string, _directiveFlag> _validDirectives;
+
     public :
         Locationconfig();
         ~Locationconfig();
@@ -20,21 +34,18 @@ class Locationconfig
 
         void setRoot(const std::string& root);
         const std::string& getRoot(void) const;
-
         void setIndex(const std::string& index);
         const std::string& getIndex(void) const;
-
         void setAutoindex(const bool& autoindex);
         const bool& getAutoindex(void) const;
-
-        void setMethod(const std::string& method);
+        void setMethod(const std::vector<std::string>& methods);
         const std::vector<std::string>& getMethods(void) const;
-
         void setCgi_pass(const std::string& cgi_pass);
         const std::string& getCgi_pass(void) const;
-
         void setRedirect(const std::string& redirect);
         const std::string& getRedirect(void) const;
+
+        bool Locationconfig::validDirective(const std::string& directive) const;
 };
 
 #endif
