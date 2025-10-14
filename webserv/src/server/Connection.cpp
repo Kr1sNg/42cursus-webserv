6aca451be29a8f587697ec90e5d18359194978d8
Theo B <theo.bahin@gmail.com>
2025-10-15 00:40:11 +0200

    resolve problem

--- webserv/src/server/Connection.cpp
+++ webserv/src/server/Connection.cpp
@@ -72,27 +72,21 @@ void	Connection::recvIntoBuffer(void) //receive !!!!
 		// _response.setReason("OK");
 		// _response.setHeader("Content-Type", "text/plain");
 		// _response.setBody("Hello from server!\n");
-		std::cout << "inside reponse" << std::endl;
 		// convert response to text
 		// _outBuf = _response.getString();
-		_outBuf =
-            "<html><body><h1>File Uploaded Successfully!</h1></body></html>";
 
-		_responseReady = true;
-		_requestReady = false;
+		_outBuf =
+					"HTTP/1.1 404 Not Found\r\n"
+					"Content-Type: text/html\r\n"
+					"Connection: close\r\n\r\n"
+					"<html><body><h1>404 - File Not Found</h1></body></html>";
 	}
-	std::cout << "inside reponse" << std::endl;
-	_outBuf =
-            "<html><body><h1>File Uploaded Successfully!</h1></body></html>";
-		_responseReady = true;
-		_requestReady = false;
 }
 
 void	Connection::flushOutBuffer(void) //send
 {
 	if (_outBuf.empty())
 		return ;
-
 	ssize_t n = send(_clientFd, _outBuf.c_str(), _outBuf.size(), 0);
 	if (n < 0)
 	{
@@ -120,8 +114,10 @@ void	Connection::handleEvent(uint32_t events)
 	}
 
 	if (events & POLLIN)
+	{
 		recvIntoBuffer();
-	
+		flushOutBuffer();
+	}
 	if (events & POLLOUT)
 		flushOutBuffer();
 }
