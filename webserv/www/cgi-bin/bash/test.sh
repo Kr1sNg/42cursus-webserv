9fc7ed7a8d78e09fd90432a27e78c6567b06e875
Kr1s Ng <piq.k0olz@gmail.com>
2025-11-12 16:14:01 +0100

    read()

--- webserv/www/cgi-bin/bash/test.sh
+++ webserv/www/cgi-bin/bash/test.sh
@@ -59,13 +59,6 @@ echo "<!DOCTYPE html>
 
 <header>
   <h1>Webserv</h1>
-  <nav>
-    <a href='/'>Home</a>
-    <a href='/uploads/'>Uploads</a>
-    <a href='/delete/'>Delete</a>
-    <a href='/form.html'>CGI</a>
-    <a href='/session/'>Session</a>
-  </nav>
 </header>
 
 <main>
